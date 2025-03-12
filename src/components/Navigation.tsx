2667eafbf45085108667b194f93c0bf63b2622d0
Anton Wolter <tech@antonwolter.de>
2025-03-12 18:10:48 +0100

    attempt to fix build

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import { ExplorationStuff } from "./ExplorationStuff";
 import { DataSettingsPopover } from "./forms/DataSettingsPopover";
-import { Toaster } from "./ui/toaster";
 
 export default function Navigation() {
   const routes = [{ title: "Streamclusters", href: "/streamclusters" }];
 
   return (
     <>
-      <Toaster />
       <nav className="bg-primary text-primary-foreground py-2">
         <div className="container flex items-center flex-wrap justify-between gap-x-6 gap-y-2">
           <div className="text-white flex items-center gap-4">
