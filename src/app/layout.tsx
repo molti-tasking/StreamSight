f9ffe7843e84350bf102fcd7f1dd1f01c11b093e
Anton Wolter <tech@antonwolter.de>
2025-03-12 17:27:39 +0100

    init posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { PostHogProvider } from "@/providers/PostHogProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col h-screen">
-          {children}
-          <Toaster />
-        </div>
+        <PostHogProvider>
+          <div className="flex flex-col h-screen">
+            {children}
+            <Toaster />
+          </div>
+        </PostHogProvider>
       </body>
     </html>
   );
